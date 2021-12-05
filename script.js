8cd16746f0da9d0485ac899ddce8114bd696c70e
mas0530 <marianasolayman@yahoo.com>
2021-12-04 19:23:06 -0600

    added some functionality

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
-$(document).ready(function () {
-    // listen for save button clicks
-    $('.saveBtn').on('click', function () {
-      // get nearby values
-      var value = $(this).siblings('.description').val();
-      var time = $(this).parent().attr('id');
-  
-      // save in localStorage
-      localStorage.setItem(time, value);
-  
-      // Show notification that item was saved to localStorage by adding class 'show'
-      $('.notification').addClass('show');
-  
-      // Timeout to remove 'show' class after 5 seconds
-      setTimeout(function () {
-        $('.notification').removeClass('show');
-      }, 5000);
-    });
-  
-    function hourUpdater() {
-      // get current number of hours
-      var currentHour = moment().hours();
-  
-      // loop over time blocks
-      $('.time-block').each(function () {
-        var blockHour = parseInt($(this).attr('id').split('-')[1]);
-  
-        // check if we've moved past this time
-        if (blockHour < currentHour) {
-          $(this).addClass('past');
-        } else if (blockHour === currentHour) {
-          $(this).removeClass('past');
-          $(this).addClass('present');
-        } else {
-          $(this).removeClass('past');
-          $(this).removeClass('present');
-          $(this).addClass('future');
-        }
-      });
+$(document).ready(function() {
+    //makes a functioning clock for current time and date
+    function updateTime() {
+        var now = moment();
+        var exactTime = now.format("MMMM Do YYYY, h:mm:ss a");
+    
+        $("#currentDay").text(exactTime);
     }
-  
-    hourUpdater();
-  
-    // set up interval to check if current time needs to be updated
-    var interval = setInterval(hourUpdater, 15000);
-  
-    // load any saved data from localStorage
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-  
-    // display current day on page
-    $('#currentDay').text(moment().format('dddd, MMMM Do'));
-  });
-  
+    setInterval(updateTime, 1000);
+    
+// provides save button functionality, and localStorage access
+    $(".saveBtn").on("click", function(){
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+    
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13  .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+// Sets (past, present, future) for time blocks
+    function timeslotAvailabiltiy(){
+        var hourCurrent = moment().hour();
+        console.log(hourCurrent)
+
+        $(".time-block").each(function(){
+            var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(hourBlock, hourCurrent)
+
+            if (hourBlock < hourCurrent){
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }else if (hourBlock === hourCurrent) {
+                $(this).addClass("present");
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+            }else{
+                $(this).addClass("future");
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+            }
+            
+        })
+    } 
+    timeslotAvailabiltiy();
+})
